b9bdc6f7d5ac444aeb16169b66215df978eb7999
Bhargav Limbani <155796549+bhargavlimbani@users.noreply.github.com>
2025-07-27 13:25:25 +0530

    Create strange_advertising.cpp

---
+++ strange_advertising.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ */
+int viralAdvertising(int n) {
+    int shared = 5;
+    int totalLikes = 0;
+
+    for (int day = 1; day <= n; ++day) {
+        int likes = shared / 2;
+        totalLikes += likes;
+        shared = likes * 3;
+    }
+
+    return totalLikes;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
