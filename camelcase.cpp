7ed0a4bd99b1383560c5f40e730ddbd66b7077f9
Bhargav Limbani <155796549+bhargavlimbani@users.noreply.github.com>
2025-11-26 20:24:23 +0530

    Implement camelcase function to count uppercase letters

---
+++ camelcase.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int camelcase(string s) {
+    int count = 1;
+    for (char c : s) {
+        if (c >= 'A' && c <= 'Z') count++;
+    }
+    return count;
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+    string s;
+    getline(cin, s);
+    int result = camelcase(s);
+    fout << result << "\n";
+    fout.close();
+    return 0;
+}
